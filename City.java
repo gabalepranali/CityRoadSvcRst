994d1198a7aa5820ab4be107d56b972a8e83e8c6
gabalepranali <gabalepranali@gmail.com>
2024-07-27 11:57:38 +0530

    Create City.java

---
+++ City.java
@@ -0,0 +1,29 @@
+public class City {
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public City(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + latitude + ", " + longitude + ")";
+    }
+}
